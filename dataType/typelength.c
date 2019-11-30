31841d618432db055a4198502c73843e672e846d
zaofengchen <zaofengchen@163.com>
2019-11-30 15:19:58 +0800

    type length

---
+++ dataType/typelength.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("size of short=%lu\n",sizeof(short));
+    printf("size of int=%lu\n",sizeof(int));
+    printf("size of long=%lu\n",sizeof(long));
+    printf("size of long=%lu\n",sizeof(long));
+}
