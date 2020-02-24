4dbdb7580590014699d919ebb32d6d95cba7adb4
zaofengchen <zaofengchen@163.com>
2020-02-24 16:13:18 +0800

    数据结构

---
+++ ds/table/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define MaxSize 50
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType data[MaxSize];
+    int length;
+} SqList;
+
+
+
+void main()
+{
+    printf("main");
+}
