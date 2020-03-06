c00fddf8c6c1be3845e68b7455287d2e312245e0
zaofengchen <zaofengchen@163.com>
2020-03-06 15:15:11 +0800

    线性表的顺序存储和链式存储

--- ds/table/mainLink.c
+++ ds/table/mainLink.c
@@ -1,47 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <malloc.h>
 typedef int ElemType;
-typedef struct LNode
+typedef struct LNode //定义单链表结点类型
 {
     ElemType data;
-    struct LNode *next;
+    struct LNode *next; //指向后继结点
 } LinkList;
 
-void CreateListF(LinkList **L,ElemType a[],int n);
-void CreateListR(LinkList *L,ElemType a[],int n);
-void DestroyList(LinkList *L);
-void DispList(LinkList L);
+void CreateListF(LinkList **L, ElemType a[], int n); //头插法建立单链表
+void CreateListR(LinkList **L, ElemType a[], int n); //尾插法建立单链表
+void DestroyList(LinkList **L);                      //销毁单链表
+void DispList(LinkList *L);                          //输出单链表
 
 int main()
 {
-    LinkList L1;
-    int a[8] = {1,3,5,7,9,2,4,6,8};
-    CreateListF(&L1,a,8);
-    // DispList(L1);
+    LinkList *L1, *L2;
+    ElemType a[] = {7, 9, 8, 2, 0, 4, 6, 3};
+    int n = 8;
+    CreateListF(&L1, a, n);
+    printf("头插法建表结果：");
+    DispList(L1);
+    CreateListR(&L2, a, n);
+    printf("尾插法建表结果：");
+    DispList(L2);
+    DestroyList(&L1);
+    DestroyList(&L2);
     return 0;
 }
 
-void CreateListF(LinkList **L,ElemType a[],int n)
+//头插法建立单链表
+void CreateListF(LinkList **L, ElemType a[], int n) 
 {
-    int i;
     LinkList *s;
-    *L = (LinkList *)malloc(sizeof(LinkList));
-    *L->next = NULL;
-    for ( i = 0; i < n; i++)
+    int i;
+    *L = (LinkList *)malloc(sizeof(LinkList)); //创建头结点
+    (*L)->next = NULL;
+    for (i = 0; i < n; i++)
     {
-        s = (LinkList *)malloc(sizeof(LinkList));
+        s = (LinkList *)malloc(sizeof(LinkList)); //创建新结点
         s->data = a[i];
-        s->next = L->next;
-        L->next = s;
+        s->next = (*L)->next; //将*s插在原开始结点之前,头结点之后
+        (*L)->next = s;
     }
-} 
+}
 
-void DispList(LinkList L)
+//尾插法建立单链表
+void CreateListR(LinkList **L, ElemType a[], int n) 
 {
-    while (L.next)
+    LinkList *s, *r;
+    int i;
+    *L = (LinkList *)malloc(sizeof(LinkList)); //创建头结点
+    (*L)->next = NULL;
+    r = *L; //r始终指向终端结点,开始时指向头结点
+    for (i = 0; i < n; i++)
     {
-        printf("%d\t",L.data);
+        s = (LinkList *)malloc(sizeof(LinkList)); //创建新结点
+        s->data = a[i];
+        r->next = s; //将*s插入*r之后
+        r = s;
+    }
+    r->next = NULL; //终端结点next域置为NULL
+}
+
+//销毁单链表
+void DestroyList(LinkList **L) 
+{
+    LinkList *p = *L, *q = p->next;
+    while (q != NULL)
+    {
+        free(p);
+        p = q;
+        q = p->next;
+    }
+    free(p); //此时q为NULL,p指向尾结点,释放它
+}
+
+//输出单链表
+void DispList(LinkList *L) 
+{
+    LinkList *p = L->next;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
     }
     printf("\n");
 }
