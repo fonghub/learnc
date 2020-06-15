2fbfd43031ad0d69fac3b383b35260217c239a4d
zaofengchen <zaofengchen@163.com>
2020-06-15 11:03:58 +0800

    1300

--- leetcode/1300.c
+++ leetcode/1300.c
@@ -2,52 +2,17 @@
 #include <stdlib.h>
 
 void bubble(int* arr, int arrSize);
-int findBestValue1(int* arr, int arrSize, int target);
-int findBestValue2(int* arr, int arrSize, int target);
-void getMMS(int* arr,int arrSize,int* maxValue,int* sumValue);
-
+int findBestValue(int* arr, int arrSize, int target);
 
 int main()
 {
-    int arr[] = {4,3,9};
+    int arr[] = {4,9,3};
     int n = 3,target = 10;
-    int value = findBestValue1(&arr[0],n,target);
+    int value = findBestValue(&arr[0],n,target);
     printf("value=%d",value);
     return 0;
 }
-
-void getMMS(int* arr,int arrSize,int* maxValue,int* sumValue)
-{
-    *maxValue = arr[0];
-    *sumValue = arr[0];
-    for(int i=1;i<arrSize;i++)
-    {
-        if(arr[i] > *maxValue) *maxValue = arr[i];
-        *sumValue += arr[i];
-    }
-}
-
-int findBestValue2(int* arr, int arrSize, int target)
-{
-    bubble(&arr[0],arrSize);
-    int sum = 0;
-    for (int i = 0; i < arrSize; i++) {
-        int x = (target - sum) / (arrSize - i);
-        if (x < arr[i]) {
-            double t = ((double)(target - sum))/(arrSize - i);
-            if (t - x > 0.5) {
-                return x + 1;
-            } else {
-                return x;
-            }
-        }
-        sum += arr[i];
-    }
-    return arr[arrSize - 1];
-
-}
-
-//  冒泡排序
+//  排序
 void bubble(int* arr, int arrSize)
 {
     for(int i=0;i<arrSize-1;i++)
@@ -67,32 +32,23 @@ void bubble(int* arr, int arrSize)
     }
 }
 
-//  v1
-int findBestValue1(int* arr, int arrSize, int target)
+int findBestValue(int* arr, int arrSize, int target)
 {
-   int maxValue = 0,sumValue = 0,value = 0,offsetValue = 0;
-    getMMS(&arr[0],arrSize,&maxValue,&sumValue);
-
-    //  数组元素和小于target，返回最大数组元素
-    if(sumValue <= target) return maxValue;
-
-    //  数组元素和大于target
-    int sumTempValue = 0,diffTempValue = 0,i;
- 
     bubble(&arr[0],arrSize);
+    int i,sum=0;
 
     for(i=0;i<arrSize;i++)
     {
-        int temp = sumTempValue + arr[i]*(arrSize-i);
-        if(temp > target) break;
-        sumTempValue += arr[i];
+        if(sum + arr[i]*(arrSize-i) > target) break;
+        sum += arr[i];
     }
-    diffTempValue = target - sumTempValue;
-    value = diffTempValue/(arrSize-i);
-    offsetValue = value + 1;
-    int diffValue1,diffValue2;
-    diffValue1 = (arrSize-i)*value-diffTempValue;
-    diffValue2 = (arrSize-i)*offsetValue-diffTempValue;
-    if(abs(diffValue1) == abs(diffValue2)) return value;
-    return abs(diffValue1) > abs(diffValue2)?offsetValue:value;
+
+    if(i == arrSize) return arr[arrSize-1];
+
+    int value = (target-sum)/(arrSize-i);
+    double value_1 = (double)(target-sum)/(arrSize-i);
+    if(value_1 - value > 0.5)
+        return value+1;
+    else
+        return value;
 }
