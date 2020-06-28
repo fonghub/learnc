2d26942a8f7c4526d79733c852f0979f41ea29b9
zaofengchen <zaofengchen@163.com>
2020-06-28 22:35:15 +0800

    ..

--- leetcode/209.c
+++ leetcode/209.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
 
 int minSubArrayLen(int s, int* nums, int numsSize);
-int sumArray(int* nums, int numsSize);
 
 int main()
 {
-    int s = 14,numsSize = 6;
-    int nums[6] = {2,3,1,2,4,3};
+    int s = 3,numsSize = 2;
+    int nums[2] = {1,1};
     int length = minSubArrayLen( s, &nums[0],  numsSize);
     printf("长度最小的子数组长度length=%d",length);
     return 0;
 }
 
-int sumArray(int* nums, int numsSize)
-{
-    int sum = 0;;
-    for(int i=0;i<numsSize;i++)
-        sum += nums[i];
-    
-    return sum;
-}
-
 int minSubArrayLen(int s, int* nums, int numsSize)
 {
-    if(sumArray(&nums[0],  numsSize) < s) return 0;
-
     int length = numsSize;
     
     for(int i=0;i<numsSize;i++)
-    {
-        int sum = nums[i];
-        int j = i;
-        int tmpLength = 1;
-        while(sum < s)
+    {   
+        int sum = 0,tmpLength = 0,j;
+        for(j=i;j<numsSize && sum<s;j++)
         {
-            j++;
-            tmpLength++;
             sum += nums[j];
+            tmpLength++;
         }
+        if(sum < s)
+        {
+            if(i == 0 && j == numsSize) return 0;
+            else break;
+        }
+        
+        break;
         if(tmpLength < length) length = tmpLength;
-        //if(j = numsSize-1) break;
+        printf("i=%d,j=%d,tmpLength=%d,sum=%d\n",i,j,tmpLength,sum);
     }
 
     return length;
